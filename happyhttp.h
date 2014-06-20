e70a0017489be2cd9b139eafb1ab56812259fdbc
peryaudo <peryaudo@gmail.com>
2014-06-20 15:28:17 +0900

    Fixed indent

--- happyhttp.h
+++ happyhttp.h
@@ -59,64 +59,64 @@ typedef void (*ResponseComplete_CB)( const Response* r, void* userdata );
 
 // HTTP status codes
 enum {
-	// 1xx informational
-	CONTINUE = 100,
-	SWITCHING_PROTOCOLS = 101,
-	PROCESSING = 102,
+  // 1xx informational
+  CONTINUE = 100,
+  SWITCHING_PROTOCOLS = 101,
+  PROCESSING = 102,
 
-	// 2xx successful
-	OK = 200,
-	CREATED = 201,
-	ACCEPTED = 202,
-	NON_AUTHORITATIVE_INFORMATION = 203,
-	NO_CONTENT = 204,
-	RESET_CONTENT = 205,
-	PARTIAL_CONTENT = 206,
-	MULTI_STATUS = 207,
-	IM_USED = 226,
+  // 2xx successful
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NON_AUTHORITATIVE_INFORMATION = 203,
+  NO_CONTENT = 204,
+  RESET_CONTENT = 205,
+  PARTIAL_CONTENT = 206,
+  MULTI_STATUS = 207,
+  IM_USED = 226,
 
-	// 3xx redirection
-	MULTIPLE_CHOICES = 300,
-	MOVED_PERMANENTLY = 301,
-	FOUND = 302,
-	SEE_OTHER = 303,
-	NOT_MODIFIED = 304,
-	USE_PROXY = 305,
-	TEMPORARY_REDIRECT = 307,
-	
-	// 4xx client error
-	BAD_REQUEST = 400,
-	UNAUTHORIZED = 401,
-	PAYMENT_REQUIRED = 402,
-	FORBIDDEN = 403,
-	NOT_FOUND = 404,
-	METHOD_NOT_ALLOWED = 405,
-	NOT_ACCEPTABLE = 406,
-	PROXY_AUTHENTICATION_REQUIRED = 407,
-	REQUEST_TIMEOUT = 408,
-	CONFLICT = 409,
-	GONE = 410,
-	LENGTH_REQUIRED = 411,
-	PRECONDITION_FAILED = 412,
-	REQUEST_ENTITY_TOO_LARGE = 413,
-	REQUEST_URI_TOO_LONG = 414,
-	UNSUPPORTED_MEDIA_TYPE = 415,
-	REQUESTED_RANGE_NOT_SATISFIABLE = 416,
-	EXPECTATION_FAILED = 417,
-	UNPROCESSABLE_ENTITY = 422,
-	LOCKED = 423,
-	FAILED_DEPENDENCY = 424,
-	UPGRADE_REQUIRED = 426,
+  // 3xx redirection
+  MULTIPLE_CHOICES = 300,
+  MOVED_PERMANENTLY = 301,
+  FOUND = 302,
+  SEE_OTHER = 303,
+  NOT_MODIFIED = 304,
+  USE_PROXY = 305,
+  TEMPORARY_REDIRECT = 307,
+  
+  // 4xx client error
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  PAYMENT_REQUIRED = 402,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  NOT_ACCEPTABLE = 406,
+  PROXY_AUTHENTICATION_REQUIRED = 407,
+  REQUEST_TIMEOUT = 408,
+  CONFLICT = 409,
+  GONE = 410,
+  LENGTH_REQUIRED = 411,
+  PRECONDITION_FAILED = 412,
+  REQUEST_ENTITY_TOO_LARGE = 413,
+  REQUEST_URI_TOO_LONG = 414,
+  UNSUPPORTED_MEDIA_TYPE = 415,
+  REQUESTED_RANGE_NOT_SATISFIABLE = 416,
+  EXPECTATION_FAILED = 417,
+  UNPROCESSABLE_ENTITY = 422,
+  LOCKED = 423,
+  FAILED_DEPENDENCY = 424,
+  UPGRADE_REQUIRED = 426,
 
-	// 5xx server error
-	INTERNAL_SERVER_ERROR = 500,
-	NOT_IMPLEMENTED = 501,
-	BAD_GATEWAY = 502,
-	SERVICE_UNAVAILABLE = 503,
-	GATEWAY_TIMEOUT = 504,
-	HTTP_VERSION_NOT_SUPPORTED = 505,
-	INSUFFICIENT_STORAGE = 507,
-	NOT_EXTENDED = 510,
+  // 5xx server error
+  INTERNAL_SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+  HTTP_VERSION_NOT_SUPPORTED = 505,
+  INSUFFICIENT_STORAGE = 507,
+  NOT_EXTENDED = 510,
 };
 
 //-------------------------------------------------
@@ -128,89 +128,89 @@ enum {
 
 class Connection
 {
-	friend class Response;
+  friend class Response;
 public:
-	// doesn't connect immediately
-	Connection( const char* host, int port );
-	~Connection();
+  // doesn't connect immediately
+  Connection( const char* host, int port );
+  ~Connection();
 
-	// Set up the response handling callbacks. These will be invoked during
-	// calls to pump().
-	// begincb		- called when the responses headers have been received
-	// datacb		- called repeatedly to handle body data
-	// completecb	- response is completed
-	// userdata is passed as a param to all callbacks.
-	void setcallbacks(
-		ResponseBegin_CB begincb,
-		ResponseData_CB datacb,
-		ResponseComplete_CB completecb,
-		void* userdata );
+  // Set up the response handling callbacks. These will be invoked during
+  // calls to pump().
+  // begincb    - called when the responses headers have been received
+  // datacb    - called repeatedly to handle body data
+  // completecb  - response is completed
+  // userdata is passed as a param to all callbacks.
+  void setcallbacks(
+    ResponseBegin_CB begincb,
+    ResponseData_CB datacb,
+    ResponseComplete_CB completecb,
+    void* userdata );
 
-	// Don't need to call connect() explicitly as issuing a request will
-	// call it automatically if needed.
-	// But it could block (for name lookup etc), so you might prefer to
-	// call it in advance.
-	Error connect();
+  // Don't need to call connect() explicitly as issuing a request will
+  // call it automatically if needed.
+  // But it could block (for name lookup etc), so you might prefer to
+  // call it in advance.
+  Error connect();
 
-	// close connection, discarding any pending requests.
-	void close();
+  // close connection, discarding any pending requests.
+  void close();
 
-	// Update the connection (non-blocking)
-	// Just keep calling this regularly to service outstanding requests.
-	Error pump();
+  // Update the connection (non-blocking)
+  // Just keep calling this regularly to service outstanding requests.
+  Error pump();
 
-	// any requests still outstanding?
-	bool outstanding() const
-		{ return !m_Outstanding.empty(); }
+  // any requests still outstanding?
+  bool outstanding() const
+    { return !m_Outstanding.empty(); }
 
-	// ---------------------------
-	// high-level request interface
-	// ---------------------------
-	
-	// method is "GET", "POST" etc...
-	// url is only path part: eg  "/index.html"
-	// headers is array of name/value pairs, terminated by a null-ptr
-	// body & bodysize specify body data of request (eg values for a form)
-	Error request( const char* method, const char* url, const char* headers[]=0,
-		const unsigned char* body=0, int bodysize=0 );
+  // ---------------------------
+  // high-level request interface
+  // ---------------------------
+  
+  // method is "GET", "POST" etc...
+  // url is only path part: eg  "/index.html"
+  // headers is array of name/value pairs, terminated by a null-ptr
+  // body & bodysize specify body data of request (eg values for a form)
+  Error request( const char* method, const char* url, const char* headers[]=0,
+    const unsigned char* body=0, int bodysize=0 );
 
-	// ---------------------------
-	// low-level request interface
-	// ---------------------------
+  // ---------------------------
+  // low-level request interface
+  // ---------------------------
 
-	// begin request
-	// method is "GET", "POST" etc...
-	// url is only path part: eg  "/index.html"
-	Error putrequest( const char* method, const char* url );
+  // begin request
+  // method is "GET", "POST" etc...
+  // url is only path part: eg  "/index.html"
+  Error putrequest( const char* method, const char* url );
 
-	// Add a header to the request (call after putrequest() )
-	Error putheader( const char* header, const char* value );
-	Error putheader( const char* header, int numericvalue );	// alternate version
+  // Add a header to the request (call after putrequest() )
+  Error putheader( const char* header, const char* value );
+  Error putheader( const char* header, int numericvalue );  // alternate version
 
-	// Finished adding headers, issue the request.
-	Error endheaders();
+  // Finished adding headers, issue the request.
+  Error endheaders();
 
-	// send body data if any.
-	// To be called after endheaders()
-	Error send( const unsigned char* buf, int numbytes );
+  // send body data if any.
+  // To be called after endheaders()
+  Error send( const unsigned char* buf, int numbytes );
 
 protected:
-	// some bits of implementation exposed to Response class
+  // some bits of implementation exposed to Response class
 
-	// callbacks
-	ResponseBegin_CB	m_ResponseBeginCB;
-	ResponseData_CB		m_ResponseDataCB;
-	ResponseComplete_CB	m_ResponseCompleteCB;
-	void*				m_UserData;
+  // callbacks
+  ResponseBegin_CB  m_ResponseBeginCB;
+  ResponseData_CB    m_ResponseDataCB;
+  ResponseComplete_CB  m_ResponseCompleteCB;
+  void*        m_UserData;
 
 private:
-	enum { IDLE, REQ_STARTED, REQ_SENT } m_State;
-	std::string m_Host;
-	int m_Port;
-	int m_Sock;
-	std::vector< std::string > m_Buffer;	// lines of request
+  enum { IDLE, REQ_STARTED, REQ_SENT } m_State;
+  std::string m_Host;
+  int m_Port;
+  int m_Sock;
+  std::vector< std::string > m_Buffer;  // lines of request
 
-	std::deque< Response* > m_Outstanding;	// responses for outstanding requests
+  std::deque< Response* > m_Outstanding;  // responses for outstanding requests
 };
 
 
@@ -227,87 +227,87 @@ private:
 
 class Response
 {
-	friend class Connection;
+  friend class Connection;
 public:
 
-	// retrieve a header (returns 0 if not present)
-	const char* getheader( const char* name ) const;
+  // retrieve a header (returns 0 if not present)
+  const char* getheader( const char* name ) const;
 
-	bool completed() const
-		{ return m_State == COMPLETE; }
+  bool completed() const
+    { return m_State == COMPLETE; }
 
 
-	// get the HTTP status code
-	int getstatus() const;
+  // get the HTTP status code
+  int getstatus() const;
 
-	// get the HTTP response reason string
-	const char* getreason() const;
+  // get the HTTP response reason string
+  const char* getreason() const;
 
-	// true if connection is expected to close after this response.
-	bool willclose() const
-		{ return m_WillClose; }
+  // true if connection is expected to close after this response.
+  bool willclose() const
+    { return m_WillClose; }
 protected:
-	// interface used by Connection
+  // interface used by Connection
 
-	// only Connection creates Responses.
-	Response( const char* method, Connection& conn );
+  // only Connection creates Responses.
+  Response( const char* method, Connection& conn );
 
-	// pump some data in for processing.
-	// Returns the number of bytes used.
-	// Will always return 0 when response is complete.
-	int pump( const unsigned char* data, int datasize, Error& err );
+  // pump some data in for processing.
+  // Returns the number of bytes used.
+  // Will always return 0 when response is complete.
+  int pump( const unsigned char* data, int datasize, Error& err );
 
-	// tell response that connection has closed
-	Error notifyconnectionclosed();
+  // tell response that connection has closed
+  Error notifyconnectionclosed();
 
 private:
-	enum {
-		STATUSLINE,		// start here. status line is first line of response.
-		HEADERS,		// reading in header lines
-		BODY,			// waiting for some body data (all or a chunk)
-		CHUNKLEN,		// expecting a chunk length indicator (in hex)
-		CHUNKEND,		// got the chunk, now expecting a trailing blank line
-		TRAILERS,		// reading trailers after body.
-		COMPLETE,		// response is complete!
-	} m_State;
+  enum {
+    STATUSLINE,    // start here. status line is first line of response.
+    HEADERS,    // reading in header lines
+    BODY,      // waiting for some body data (all or a chunk)
+    CHUNKLEN,    // expecting a chunk length indicator (in hex)
+    CHUNKEND,    // got the chunk, now expecting a trailing blank line
+    TRAILERS,    // reading trailers after body.
+    COMPLETE,    // response is complete!
+  } m_State;
 
-	Connection& m_Connection;	// to access callback ptrs
-	std::string m_Method;		// req method: "GET", "POST" etc...
+  Connection& m_Connection;  // to access callback ptrs
+  std::string m_Method;    // req method: "GET", "POST" etc...
 
-	// status line
-	std::string	m_VersionString;	// HTTP-Version
-	int	m_Version;			// 10: HTTP/1.0    11: HTTP/1.x (where x>=1)
-	int m_Status;			// Status-Code
-	std::string m_Reason;	// Reason-Phrase
+  // status line
+  std::string  m_VersionString;  // HTTP-Version
+  int  m_Version;      // 10: HTTP/1.0    11: HTTP/1.x (where x>=1)
+  int m_Status;      // Status-Code
+  std::string m_Reason;  // Reason-Phrase
 
-	// header/value pairs
-	std::map<std::string,std::string> m_Headers;
+  // header/value pairs
+  std::map<std::string,std::string> m_Headers;
 
-	int		m_BytesRead;		// body bytes read so far
-	bool	m_Chunked;			// response is chunked?
-	int		m_ChunkLeft;		// bytes left in current chunk
-	int		m_Length;			// -1 if unknown
-	bool	m_WillClose;		// connection will close at response end?
+  int    m_BytesRead;    // body bytes read so far
+  bool  m_Chunked;      // response is chunked?
+  int    m_ChunkLeft;    // bytes left in current chunk
+  int    m_Length;      // -1 if unknown
+  bool  m_WillClose;    // connection will close at response end?
 
-	std::string m_LineBuf;		// line accumulation for states that want it
-	std::string m_HeaderAccum;	// accumulation buffer for headers
+  std::string m_LineBuf;    // line accumulation for states that want it
+  std::string m_HeaderAccum;  // accumulation buffer for headers
 
 
-	void FlushHeader();
-	Error ProcessStatusLine( std::string const& line );
-	void ProcessHeaderLine( std::string const& line );
-	void ProcessTrailerLine( std::string const& line );
-	void ProcessChunkLenLine( std::string const& line );
+  void FlushHeader();
+  Error ProcessStatusLine( std::string const& line );
+  void ProcessHeaderLine( std::string const& line );
+  void ProcessTrailerLine( std::string const& line );
+  void ProcessChunkLenLine( std::string const& line );
 
-	int ProcessDataChunked( const unsigned char* data, int count );
-	int ProcessDataNonChunked( const unsigned char* data, int count );
+  int ProcessDataChunked( const unsigned char* data, int count );
+  int ProcessDataNonChunked( const unsigned char* data, int count );
 
-	void BeginBody();
-	bool CheckClose();
-	void Finish();
+  void BeginBody();
+  bool CheckClose();
+  void Finish();
 };
 
-}	// end namespace happyhttp
+}  // end namespace happyhttp
 
 
 #endif // HAPPYHTTP_H
--- test.cpp
+++ test.cpp
@@ -10,36 +10,36 @@ int count=0;
 
 void OnBegin( const happyhttp::Response* r, void* userdata )
 {
-	printf( "BEGIN (%d %s)\n", r->getstatus(), r->getreason() );
-	count = 0;
+  printf( "BEGIN (%d %s)\n", r->getstatus(), r->getreason() );
+  count = 0;
 }
 
 void OnData( const happyhttp::Response* r, void* userdata, const unsigned char* data, int n )
 {
-	fwrite( data,1,n, stdout );
-	count += n;
+  fwrite( data,1,n, stdout );
+  count += n;
 }
 
 void OnComplete( const happyhttp::Response* r, void* userdata )
 {
-	printf( "COMPLETE (%d bytes)\n", count );
+  printf( "COMPLETE (%d bytes)\n", count );
 }
 
 
 
 happyhttp::Error Test1()
 {
-	puts("-----------------Test1------------------------" );
-	// simple simple GET
-	happyhttp::Connection conn( "scumways.com", 80 );
-	conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
+  puts("-----------------Test1------------------------" );
+  // simple simple GET
+  happyhttp::Connection conn( "scumways.com", 80 );
+  conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
 
-	if (happyhttp::Error err = conn.request( "GET", "/happyhttp/test.php", 0, 0,0 )) {
+  if (happyhttp::Error err = conn.request( "GET", "/happyhttp/test.php", 0, 0,0 )) {
     return err;
   }
 
-	while( conn.outstanding() ) {
-		if (happyhttp::Error err = conn.pump()) {
+  while( conn.outstanding() ) {
+    if (happyhttp::Error err = conn.pump()) {
       return err;
     }
   }
@@ -51,31 +51,31 @@ happyhttp::Error Test1()
 
 happyhttp::Error Test2()
 {
-	puts("-----------------Test2------------------------" );
-	// POST using high-level request interface
+  puts("-----------------Test2------------------------" );
+  // POST using high-level request interface
 
-	const char* headers[] = 
-	{
-		"Connection", "close",
-		"Content-type", "application/x-www-form-urlencoded",
-		"Accept", "text/plain",
-		0
-	};
+  const char* headers[] = 
+  {
+    "Connection", "close",
+    "Content-type", "application/x-www-form-urlencoded",
+    "Accept", "text/plain",
+    0
+  };
 
-	const char* body = "answer=42&name=Bubba";
-	
-	happyhttp::Connection conn( "scumways.com", 80 );
-	conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
-	if (happyhttp::Error err = conn.request( "POST",
-			"/happyhttp/test.php",
-			headers,
-			(const unsigned char*)body,
-			strlen(body) )) {
+  const char* body = "answer=42&name=Bubba";
+  
+  happyhttp::Connection conn( "scumways.com", 80 );
+  conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
+  if (happyhttp::Error err = conn.request( "POST",
+      "/happyhttp/test.php",
+      headers,
+      (const unsigned char*)body,
+      strlen(body) )) {
     return err;
   }
 
-	while( conn.outstanding() ) {
-		if (happyhttp::Error err = conn.pump()) {
+  while( conn.outstanding() ) {
+    if (happyhttp::Error err = conn.pump()) {
       return err;
     }
   }
@@ -85,25 +85,25 @@ happyhttp::Error Test2()
 
 happyhttp::Error Test3()
 {
-	puts("-----------------Test3------------------------" );
-	// POST example using lower-level interface
+  puts("-----------------Test3------------------------" );
+  // POST example using lower-level interface
 
-	const char* params = "answer=42&foo=bar";
-	int l = strlen(params);
+  const char* params = "answer=42&foo=bar";
+  int l = strlen(params);
 
-	happyhttp::Connection conn( "scumways.com", 80 );
-	conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
+  happyhttp::Connection conn( "scumways.com", 80 );
+  conn.setcallbacks( OnBegin, OnData, OnComplete, 0 );
 
-	if (happyhttp::Error err = conn.putrequest( "POST", "/happyhttp/test.php" )) return err;
-	if (happyhttp::Error err = conn.putheader( "Connection", "close" )) return err;
-	if (happyhttp::Error err = conn.putheader( "Content-Length", l )) return err;
-	if (happyhttp::Error err = conn.putheader( "Content-type", "application/x-www-form-urlencoded" )) return err;
-	if (happyhttp::Error err = conn.putheader( "Accept", "text/plain" )) return err;
-	if (happyhttp::Error err = conn.endheaders()) return err;
-	if (happyhttp::Error err = conn.send( (const unsigned char*)params, l )) return err;
+  if (happyhttp::Error err = conn.putrequest( "POST", "/happyhttp/test.php" )) return err;
+  if (happyhttp::Error err = conn.putheader( "Connection", "close" )) return err;
+  if (happyhttp::Error err = conn.putheader( "Content-Length", l )) return err;
+  if (happyhttp::Error err = conn.putheader( "Content-type", "application/x-www-form-urlencoded" )) return err;
+  if (happyhttp::Error err = conn.putheader( "Accept", "text/plain" )) return err;
+  if (happyhttp::Error err = conn.endheaders()) return err;
+  if (happyhttp::Error err = conn.send( (const unsigned char*)params, l )) return err;
 
-	while( conn.outstanding() ) {
-		if (happyhttp::Error err = conn.pump()) return err;
+  while( conn.outstanding() ) {
+    if (happyhttp::Error err = conn.pump()) return err;
   }
   return NULL;
 }
@@ -116,11 +116,11 @@ int main( int argc, char* argv[] )
 #ifdef WIN32
     WSAData wsaData;
     int code = WSAStartup(MAKEWORD(1, 1), &wsaData);
-	if( code != 0 )
-	{
-		fprintf(stderr, "shite. %d\n",code);
-		return 0;
-	}
+  if( code != 0 )
+  {
+    fprintf(stderr, "shite. %d\n",code);
+    return 0;
+  }
 #endif //WIN32
   happyhttp::Error err = NULL;
   if ((err = Test1())) { }
@@ -129,12 +129,12 @@ int main( int argc, char* argv[] )
 
   if (err) {
     fprintf(stderr, "Error: %s\n", err->c_str());
-	}
-	
+  }
+  
 #ifdef WIN32
     WSACleanup();
 #endif // WIN32
-	return 0;
+  return 0;
 }
 
 
